308146e2a260e432c38d075b72eac24312d03176
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-01-21 22:32:29 +0530

    Show Selected tiles RPS

--- Rock-Paper-Scissor/script.js
+++ Rock-Paper-Scissor/script.js
@@ -1,28 +1,38 @@
 const allTiles = ["rock", "paper", "scissor"];
 
-const playerPoints = 0;
+let playerPoints = 0;
+let playerTile;
 const playerScore = document.querySelector("#playerScore");
 const player = document.querySelector(".player");
 const playerTiles = player.querySelectorAll(".tile");
 
-const computerPoints = 0;
+let computerPoints = 0;
 const computerScore = document.querySelector("#computerScore");
 const computer = document.querySelector(".computer");
 const computerTiles = computer.querySelectorAll(".tile");
 
-let selectedTile;
-let currentTiles;
-let randomTile;
+const clearSelections = () => {
+  playerTiles.forEach((tile) => tile.classList.remove("selected"));
+  computerTiles.forEach((tile) => tile.classList.remove("selected"));
+};
 
 const randomGenerator = () => {
-  let randomNum = Math.floor(Math.random() * 3);
-  return allTiles[randomNum];
+  let randomNum = Math.floor(Math.random() * 30) + 1;
+  return allTiles[randomNum % 3];
+};
+
+const showSelections = () => {
+  player.querySelector(`[data-type="${playerTile}"]`).classList.add("selected");
+  computer
+    .querySelector(`[data-type="${computerTile}"]`)
+    .classList.add("selected");
 };
 
 const play = (event) => {
-  selectedTile = event.target.dataset.type;
-  randomTile = randomGenerator();
-  console.log(selectedTile + "  " + randomTile);
+  clearSelections();
+  playerTile = event.target.dataset.type;
+  computerTile = randomGenerator();
+  showSelections();
 };
 
 playerTiles.forEach((tile) => {
