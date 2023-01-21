44cdef694bd6e4b92006d2d3586c72e8639554ec
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-01-21 22:01:36 +0530

    ButtonClick implement RPS

--- Rock-Paper-Scissor/script.js
+++ Rock-Paper-Scissor/script.js
@@ -1,1 +1,30 @@
-console.log("Hello");
+const allTiles = ["rock", "paper", "scissor"];
+
+const playerPoints = 0;
+const playerScore = document.querySelector("#playerScore");
+const player = document.querySelector(".player");
+const playerTiles = player.querySelectorAll(".tile");
+
+const computerPoints = 0;
+const computerScore = document.querySelector("#computerScore");
+const computer = document.querySelector(".computer");
+const computerTiles = computer.querySelectorAll(".tile");
+
+let selectedTile;
+let currentTiles;
+let randomTile;
+
+const randomGenerator = () => {
+  let randomNum = Math.floor(Math.random() * 3);
+  return allTiles[randomNum];
+};
+
+const play = (event) => {
+  selectedTile = event.target.dataset.type;
+  randomTile = randomGenerator();
+  console.log(selectedTile + "  " + randomTile);
+};
+
+playerTiles.forEach((tile) => {
+  tile.addEventListener("click", play);
+});
