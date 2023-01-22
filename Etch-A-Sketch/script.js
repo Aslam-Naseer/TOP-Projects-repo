0d9bd8fe14e5f34f23eb2ca88312c69f586464c5
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-01-22 13:50:24 +0530

    feat: Enable clear button

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,18 +1,23 @@
 const container = document.querySelector("#container");
-let gridOrder = 64;
+let gridOrder = 32;
 let tileColor = "#FFFFFF";
-
-let gridPieces = [];
 
 const createGrid = () => {
   container.style.gridTemplateColumns = `repeat(${gridOrder}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${gridOrder}, 1fr)`;
   container.innerHTML = "";
+
+  let gridPieces = [];
   for (i = 0; i < gridOrder * gridOrder; i++) {
     gridPieces[i] = document.createElement("div");
     gridPieces[i].onmouseover = paint;
     container.appendChild(gridPieces[i]);
   }
+};
+
+const clearTiles = () => {
+  const tilesList = document.querySelectorAll("#container>div");
+  tilesList.forEach((tile) => (tile.style.backgroundColor = "inherit"));
 };
 
 const paint = (e) => {
