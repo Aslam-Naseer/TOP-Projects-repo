124ac085f2cad1e5d016dda81aff7aa6ba5187aa
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-01-26 17:24:54 +0530

    feat: Add number buttons functionality

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,22 @@
+const calculator = document.querySelector("main");
 
+const output = calculator.querySelector("#output");
+const numButtons = calculator.querySelectorAll(".num");
+const operators = calculator.querySelectorAll(".opr");
+const equalBtn = calculator.querySelector(".eql");
+const zero = calculator.querySelector(".zero");
+
+let value1, value2, operation;
+
+const numAction = (e) => {
+  if (zero.disabled) {
+    output.textContent = "";
+    zero.disabled = false;
+  }
+  if (output.textContent.length < 10) {
+    output.textContent = output.textContent + e.target.value;
+  }
+};
+
+numButtons.forEach((btn) => btn.addEventListener("click", numAction));
+//operators.forEach((btn) => btn.addEventListener("click", oprAction));
