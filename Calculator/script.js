a38d48d888ddf8121b0a3978486f1d99233c746d
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-01-26 18:55:03 +0530

    Add basic operations

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,12 +1,35 @@
 const calculator = document.querySelector("main");
 
 const output = calculator.querySelector("#output");
+const miniOut = calculator.querySelector("#miniOut");
 const numButtons = calculator.querySelectorAll(".num");
 const operators = calculator.querySelectorAll(".opr");
-const equalBtn = calculator.querySelector(".eql");
+const equalBtn = calculator.querySelector(".equal");
 const zero = calculator.querySelector(".zero");
 
-let value1, value2, operation;
+let input = 0,
+  result = 0,
+  operation;
+
+const equate = () => {
+  input = parseFloat(output.textContent);
+  switch (operation) {
+    case "+":
+      result += input;
+      break;
+    case "-":
+      result -= input;
+      break;
+    case "x":
+      result *= input;
+      break;
+    case "/":
+      result /= input;
+      break;
+    default:
+      result = input;
+  }
+};
 
 const numAction = (e) => {
   if (zero.disabled) {
@@ -14,9 +37,29 @@ const numAction = (e) => {
     zero.disabled = false;
   }
   if (output.textContent.length < 10) {
-    output.textContent = output.textContent + e.target.value;
+    output.textContent += e.target.value;
   }
 };
 
+const oprAction = (e) => {
+  equate();
+  operation = e.target.value;
+  miniOut.textContent = result + "  " + operation;
+  output.textContent = 0;
+  equalBtn.disabled = false;
+  zero.disabled = true;
+};
+
+const equalAction = (e) => {
+  const temp = result;
+  equate();
+  miniOut.textContent = temp + " " + operation + " " + input;
+  output.textContent = result;
+  operation = "";
+  equalBtn.disabled = true;
+  zero.disabled = true;
+};
+
 numButtons.forEach((btn) => btn.addEventListener("click", numAction));
-//operators.forEach((btn) => btn.addEventListener("click", oprAction));
+operators.forEach((btn) => btn.addEventListener("click", oprAction));
+equalBtn.addEventListener("click", equalAction);
