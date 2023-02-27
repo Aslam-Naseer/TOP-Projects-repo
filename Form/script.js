bf8cce22f2867836b4c256c2cc9e098920eb03e8
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-02-27 17:41:21 +0530

    Add validation events

--- Form/script.js
+++ Form/script.js
@@ -2,16 +2,57 @@ const form = document.querySelector("form");
 const inputs = document.querySelectorAll("input");
 const submitBtn = document.querySelector(".submit-btn");
 
-const validityTest = (node) => {
-  node.classList.add("input-box");
-};
+let flag = true;
 
 const validateAll = (event) => {
-  inputs.forEach((item) => {
-    if (!validityTest(item)) {
+  if (flag) {
+    inputs.forEach((item) => item.classList.add("input-box"));
+    flag = false;
+  }
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].checkValidity() === false) {
+      inputs[i].reportValidity();
       event.preventDefault();
+      break;
     }
-  });
+  }
 };
 
 form.addEventListener("submit", validateAll);
+
+inputs[2].addEventListener("keyup", (event) => {
+  const elem = event.target;
+
+  if (elem.validity.typeMismatch) {
+    elem.setCustomValidity("I am expecting an email address!");
+  } else {
+    elem.setCustomValidity("");
+  }
+});
+
+inputs[4].addEventListener("keyup", (event) => {
+  const elem = event.target;
+
+  if (elem.validity.tooShort) {
+    elem.setCustomValidity(`Only ${elem.value.length} / 8 characters `);
+  } else {
+    elem.setCustomValidity("");
+  }
+});
+
+inputs[5].addEventListener("keyup", (event) => {
+  const elem = event.target;
+
+  if (elem.value !== inputs[4].value) {
+    elem.setCustomValidity("Password mismatch");
+  } else {
+    elem.setCustomValidity("");
+  }
+});
+
+inputs.forEach((node) =>
+  node.addEventListener("focusout", (node) =>
+    node.target.classList.add("input-box")
+  )
+);
