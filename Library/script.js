928538d6ba4443375d5f190c35250021fde72ca9
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-03-10 18:52:22 +0530

    Ad overlay base

--- Library/script.js
+++ Library/script.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+const addBtn = document.querySelector(".add-more");
+const overlay = document.querySelector(".overlay");
+
+const popup = () => {
+  overlay.style.display = "block";
+  addBtn.classList.add("add-more-hover");
+};
+
+const removeOverlay = () => {
+  overlay.style.display = "none";
+  addBtn.classList.remove("add-more-hover");
+};
+
+addBtn.addEventListener("click", popup);
+overlay.addEventListener("click", removeOverlay);
