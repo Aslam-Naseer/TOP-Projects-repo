1e0a84517aaa86f74f42c41702c1ce3c7e79a1c6
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-03-10 20:12:55 +0530

    Add new book panel

--- Library/script.js
+++ Library/script.js
@@ -1,15 +1,19 @@
 const addBtn = document.querySelector(".add-more");
 const overlay = document.querySelector(".overlay");
+const newPanel = document.querySelector(".new-book-panel");
 
 const popup = () => {
-  overlay.style.display = "block";
+  overlay.style.visibility = "visible";
   addBtn.classList.add("add-more-hover");
 };
 
 const removeOverlay = () => {
-  overlay.style.display = "none";
+  overlay.style.visibility = "hidden";
   addBtn.classList.remove("add-more-hover");
 };
 
 addBtn.addEventListener("click", popup);
 overlay.addEventListener("click", removeOverlay);
+newPanel.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
