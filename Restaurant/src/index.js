6fd645051f82469534155d44ba6ecf89acabe0a6
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-03-25 14:30:57 +0530

    Load assets

--- Restaurant/src/index.js
+++ Restaurant/src/index.js
@@ -1,1 +1,13 @@
-console.log("Init");
+import "./style.css";
+
+const content = document.querySelector("#content");
+
+function component() {
+  const element = document.createElement("div");
+
+  element.textContent = "Hello WebPack";
+
+  return element;
+}
+
+document.body.appendChild(component());
