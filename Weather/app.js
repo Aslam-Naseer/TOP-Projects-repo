7f5852933fca2016e4a9a07f3c5643404c7886e0
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-04-14 12:40:06 +0530

    Add fetch operation

--- Weather/app.js
+++ Weather/app.js
@@ -1,1 +1,29 @@
-console.log("Init");
+const form = document.querySelector(".header-form");
+const locationInput = form.querySelector(".search-box");
+
+function fnc() {
+  throw new Error("LL");
+}
+
+const fetchData = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=ec6f8969587147e9bcb134400230804&q=${locationInput.value}`
+    );
+
+    const data = await response.json();
+
+    if (!data.error) {
+      console.log(data);
+      locationInput.value = "";
+    } else {
+      throw new Error("unnamed location");
+    }
+  } catch (err) {
+    console.error(`error: ${err}`);
+  }
+};
+
+form.addEventListener("submit", fetchData);
