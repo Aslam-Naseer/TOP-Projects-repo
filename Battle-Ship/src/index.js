2891a108ce81ba1de0128ae233e9cee80ec2bbd5
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-04-27 23:57:33 +0530

    Final commit -Battleship

--- Battle-Ship/src/index.js
+++ Battle-Ship/src/index.js
@@ -1,6 +1,4 @@
 import domMethods from "./domStuff";
-import { play, obj, placeShip, randomPlace, newGame } from "./game";
-import { player } from "./player";
-import ship from "./shipFactory";
+import { newGame } from "./game";
 
 newGame();
